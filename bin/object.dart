c2ede1559b597d2a4ed7ca5f64d52499f24c6d1a
Ari Surya Ramadhan <arisurya2605@gmail.com>
2023-12-24 20:26:36 +0700

    Object

---
+++ bin/object.dart
@@ -0,0 +1,14 @@
+/* 
+  Object adalah hasil dari instansiasi dari sebuah class
+  Untuk membuat object kita bisa menggunakan nama class lalu diikuti dengan kurung()
+ */
+
+class Person {}
+
+void main() {
+  var person1 = Person();
+  print(person1);
+
+  Person person2 = Person();
+  print(person2);
+}
